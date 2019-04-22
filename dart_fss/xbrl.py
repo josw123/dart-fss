b6d1a317538340b476df497b2851eeb3fc7790a7
swjo <swjo@etri.re.kr>
2019-04-22 10:39:07 +0900

    macOS에서 발생하는 오류 수정(#2)

--- dart_fss/xbrl.py
+++ dart_fss/xbrl.py
@@ -736,12 +736,14 @@ def get_xbrl_from_file(file_path: str) -> DartXbrl:
     DartXbrl
         DartXbrl 클래스
     """
-    # PyPI를 통해 설치된 Arelle 라이브러리 사용시 Linux에서 발생하는 오류 수정을 위한코드
+    # PyPI를 통해 설치된 Arelle 라이브러리 사용시 발생하는 오류 수정을 위한코드
     # Github 버전에서는 수정된 상태로 이후 삭제예정
     if sys.platform == 'win32':
         pass
     elif sys.platform == 'darwin':
-        pass
+        arelle_app_dir = os.path.join(os.path.expanduser('~/Library/Application Support'), 'Arelle')
+        if not os.path.exists(arelle_app_dir):
+            os.makedirs(arelle_app_dir)
     else:
         arelle_app_dir = os.path.join(os.path.expanduser("~/.config"), "arelle")
         if not os.path.exists(arelle_app_dir):
