094ebb78dbd14f3f936d4bbf07c185e0deba86a1
josw123 <nonswing.z@gmail.com>
2020-02-09 00:01:05 +0900

    Fix typo

--- dart_fss/auth/__init__.py
+++ dart_fss/auth/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-from .auth import set_api_key, get_api_key
+from dart_fss.auth.auth import set_api_key, get_api_key
 
 __all__ = ['set_api_key', 'get_api_key']
--- dart_fss/utils/__init__.py
+++ dart_fss/utils/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-from .cache import cache
-from .datetime import get_datetime, check_datetime
-from .file import unzip, xml_to_dict, search_file, create_folder, get_cache_folder
-from .notebook import dict_to_html, is_notebook
-from .request import get_user_agent, query_to_regex, request
-from .singleton import Singleton
-from .spinner import Spinner
-from .string import str_compare, str_insert_whitespace, str_unit_to_number_unit
-from .regex import is_operator, precedence, infix_to_postfix, str_to_regex, str_to_pattern
+from dart_fss.utils.cache import cache
+from dart_fss.utils.datetime import get_datetime, check_datetime
+from dart_fss.utils.file import unzip, xml_to_dict, search_file, create_folder, get_cache_folder
+from dart_fss.utils.notebook import dict_to_html, is_notebook
+from dart_fss.utils.request import get_user_agent, query_to_regex, request
+from dart_fss.utils.singleton import Singleton
+from dart_fss.utils.spinner import Spinner
+from dart_fss.utils.string import str_compare, str_insert_whitespace, str_unit_to_number_unit
+from dart_fss.utils.regex import is_operator, precedence, infix_to_postfix, str_to_regex, str_to_pattern
 
 
 __all__ = ['cache', 'get_datetime', 'check_datetime', 'unzip', 'xml_to_dict',
