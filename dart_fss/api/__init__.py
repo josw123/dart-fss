915566a9fe0253e15bf034378a6282eb1f24b5ac
josw123 <nonswing.z@gmail.com>
2020-06-25 17:48:47 +0900

    Add API for stock market list

--- dart_fss/api/__init__.py
+++ dart_fss/api/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from dart_fss.api import filings, finance, info, shareholder
+from dart_fss.api import filings, finance, info, shareholder, market
 
 
-__all__ = ['filings', 'finance', 'info', 'shareholder']
+__all__ = ['filings', 'finance', 'info', 'shareholder', 'market']
 
---
+++ dart_fss/api/market/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from .stock_market import get_stock_market_list
+
+__all__ = ['get_stock_market_list']
