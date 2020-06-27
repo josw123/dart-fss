df914335b936e0949a8c7544c8d6943e637d3f54
josw123 <nonswing.z@gmail.com>
2020-06-28 01:07:42 +0900

    Update compatible python version

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only'
     ],
     packages=find_packages(exclude=['dart_fss.tests']),
