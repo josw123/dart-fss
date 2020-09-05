7a80fa8f0b5403e9b754408a4641b318078480c6
josw123 <nonswing.z@gmail.com>
2020-09-05 17:27:01 +0900

    Add an option to show a loading indicator

--- dart_fss/xbrl/xbrl.py
+++ dart_fss/xbrl/xbrl.py
@@ -6,7 +6,7 @@ from arelle import Cntlr
 from dart_fss.xbrl.dart_xbrl import DartXbrl
 
 
-def get_xbrl_from_file(file_path: str) -> DartXbrl:
+def get_xbrl_from_file(file_path: str, loading_indicator: bool = True) -> DartXbrl:
     """ XBRL 파일 로드 함수
 
     XBRL 파일을 로드하기 위한 함수로 로딩완료 후 DartXbrl 클래스를 반환한다
@@ -15,17 +15,20 @@ def get_xbrl_from_file(file_path: str) -> DartXbrl:
     ----------
     file_path: str
         XBRL 파일 경로
-
+    loading_indicator: bool
+        XBRL 로딩시 Spinner 표시 여부(default: True)
     Returns
     -------
     DartXbrl
         DartXbrl 클래스
     """
-    # PyPI를 통해 설치된 Arelle 라이브러리 사용시 발생하는 오류 수정을 위한코드
-    from dart_fss.utils.spinner import Spinner
-    spinner = Spinner('XBRL Loading')
-    spinner.start()
+    spinner = None
+    if loading_indicator:
+        from dart_fss.utils.spinner import Spinner
+        spinner = Spinner('XBRL Loading')
+        spinner.start()
 
+    # PyPI를 통해 설치된 Arelle 라이브러리 사용시 발생하는 오류 수정을 위한코드
     if sys.platform == 'win32':
         pass
     elif sys.platform == 'darwin':
@@ -39,5 +42,7 @@ def get_xbrl_from_file(file_path: str) -> DartXbrl:
     model_xbrl = Cntlr.Cntlr().modelManager.load(file_path)
     filename = file_path.split('\\')[-1]
     xbrl = DartXbrl(filename, model_xbrl)
-    spinner.stop()
+
+    if loading_indicator:
+        spinner.stop()
     return xbrl
