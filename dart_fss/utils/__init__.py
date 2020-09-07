5b7f1978c0d78e2f83a1e56aaad7c37d0d0a19b6
josw123 <nonswing.z@gmail.com>
2020-09-07 10:41:27 +0900

    Add option for spinner enable/disable

--- dart_fss/utils/__init__.py
+++ dart_fss/utils/__init__.py
@@ -5,7 +5,7 @@ from dart_fss.utils.file import unzip, xml_to_dict, search_file, create_folder, 
 from dart_fss.utils.notebook import dict_to_html, is_notebook
 from dart_fss.utils.request import get_user_agent, query_to_regex, request
 from dart_fss.utils.singleton import Singleton
-from dart_fss.utils.spinner import Spinner
+from dart_fss.utils.spinner import Spinner, spinner_enable
 from dart_fss.utils.string import str_compare, str_insert_whitespace, str_unit_to_number_unit, str_upper, get_currency_str
 from dart_fss.utils.regex import is_operator, precedence, infix_to_postfix, str_to_regex, str_to_pattern
 from dart_fss.utils.dataframe import dataframe_astype
@@ -14,6 +14,6 @@ from dart_fss.utils.dataframe import dataframe_astype
 __all__ = ['cache', 'get_datetime', 'check_datetime', 'unzip', 'xml_to_dict',
            'search_file', 'create_folder', 'get_cache_folder', 'dict_to_html',
            'is_notebook', 'get_user_agent', 'query_to_regex', 'request',
-           'Singleton', 'Spinner', 'str_compare', 'str_insert_whitespace',
+           'Singleton', 'Spinner', 'spinner_enable', 'str_compare', 'str_insert_whitespace',
            'str_unit_to_number_unit', 'get_currency_str', 'str_upper', 'is_operator', 'precedence',
            'infix_to_postfix', 'str_to_regex', 'str_to_pattern', 'dataframe_astype']
--- dart_fss/utils/spinner.py
+++ dart_fss/utils/spinner.py
@@ -6,24 +6,39 @@ if is_notebook():
 else:
     from halo import Halo
 
+# Global Spinner Control
+spinner_enable = True
 
-class Spinner:
-    """
-    Halo 라이브러리를 이용한 Spinner
-    """
-    def __init__(self, text):
-        """ 초기화
-        Parameters
-        ----------
-        text: str
-            spinner 사용시 표시할 text
+if spinner_enable:
+    class Spinner:
         """
-        self.spinner = Halo(text=text, spinner='dots')
+        Halo 라이브러리를 이용한 Spinner
+        """
+        def __init__(self, text):
+            """ 초기화
+            Parameters
+            ----------
+            text: str
+                spinner 사용시 표시할 text
+            """
+            self.spinner = Halo(text=text, spinner='dots')
 
-    def start(self):
-        """ Spinner Start"""
-        self.spinner.start()
+        def start(self):
+            """ Spinner Start"""
+            if spinner_enable:
+                self.spinner.start()
 
-    def stop(self):
-        """ Spinner Stop """
-        self.spinner.stop()
+        def stop(self):
+            """ Spinner Stop """
+            if spinner_enable:
+                self.spinner.stop()
+else:
+    class Spinner:
+        def __init__(self, text):
+            pass
+
+        def start(self):
+            pass
+
+        def stop(self):
+            pass
