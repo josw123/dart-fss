75a00d0e297d538ffa638ebe950678b88ff60318
swjo <nonswing.z@gmail.com>
2023-04-24 13:57:05 +0900

    Add(option): add optional dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
-from setuptools import setup, find_packages
-from os import path
+from setuptools import setup
 
 import versioneer
 
 NAME = 'dart-fss'
 
-with open(path.join('./', 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
 )
