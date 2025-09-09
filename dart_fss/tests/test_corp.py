5dcf2b7b430a3049508c8aed87af2fe6c7d17e6c
swjo <nonswing.z@gmail.com>
2025-09-09 13:51:13 +0900

    Fix: Update expected value in sector test case to reflect increased company count

--- dart_fss/tests/test_corp.py
+++ dart_fss/tests/test_corp.py
@@ -32,7 +32,7 @@ def test_find_by_product(corp_list):
 def test_find_by_sector(corp_list):
     res = corp_list.find_by_sector(corp_list.sectors[0])
     actual = len(res)
-    expected = 21 #섹터 회사수 증가로 수정
+    expected = 22  # 섹터 회사수 증가로 수정
     assert actual == expected
 
 
@@ -90,4 +90,3 @@ def test_corp_search_filings(corp_list):
     actual = filings.total_count
     expected = 1
     assert actual == expected
-
