014b91bb331a4dd0ed580db26b4be4f8601ad27c
swjo <nonswing.z@gmail.com>
2025-09-11 21:19:18 +0900

    Fix: Update test cases for revised logic and updated data
    - Adjusted `test_not_found_consolidated` to include `skip_error=False` parameter.
    - Updated expected value in `test_find_by_sector` due to corp list changes.
    - Corrected indexing syntax in `test_xbrl_get_financial_statement_information`.

--- dart_fss/tests/test_errors.py
+++ dart_fss/tests/test_errors.py
@@ -30,4 +30,4 @@ def test_errors(dart):
 def test_not_found_consolidated(dart, corp_list):
     with pytest.raises(dart.errors.NotFoundConsolidated):
         crp = corp_list.find_by_stock_code('204210')  # 종목명 변경
-        crp.extract_fs(bgn_de='20180101', end_de='20190101')
+        crp.extract_fs(bgn_de='20180101', end_de='20190101', skip_error=False)
