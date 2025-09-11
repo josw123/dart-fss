2e2e9df5c161781bcbff76a2cce0252f6827186a
swjo <nonswing.z@gmail.com>
2025-09-11 21:41:57 +0900

    Fix: Update sector test case to use a specific sector name for consistency

--- dart_fss/tests/test_corp.py
+++ dart_fss/tests/test_corp.py
@@ -30,7 +30,7 @@ def test_find_by_product(corp_list):
 
 
 def test_find_by_sector(corp_list):
-    res = corp_list.find_by_sector(corp_list.sectors[0])
+    res = corp_list.find_by_sector('1차 비철금속 제조업')
     actual = len(res)
     expected = 19  # The number may vary as the corp list is updated
     assert actual == expected
